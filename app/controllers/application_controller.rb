4c1fc9e612bf2a40976af5bcf66946fd228a60e2
Spencer Owen <owenspencer@gmail.com>
2019-06-25 17:13:01 -0600

    Goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   def hello
-    render html: "hello, world!"
+    render html: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye world"
   end
 end
